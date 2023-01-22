50f330cacbe495adf3fff283ffcdc8a20098a046
kamilchulakov <mr1english4@gmail.com>
2023-01-22 17:07:44 +0300

    LAB1: fix tests

--- lab1/src/test/test_document.c
+++ lab1/src/test/test_document.c
@@ -1,7 +1,8 @@
 #include "test_document.h"
 #include "../main/document.h"
 
-test_status test_document(FILE* fp) {
+test_status test_document1(FILE* fp) {
+    print_running_test("test_document1");
     document* doc = create_document(1);
     doc->data = create_element_int32("8B", 64);
     open_test_file_write(fp);
@@ -11,5 +12,11 @@ test_status test_document(FILE* fp) {
     assert(read_document(fp, doc) == READ_OK);
     element* expected_el = create_element_int32("8B", 64);
     assert_element_equals( doc->data, expected_el);
+    return TEST_OK;
+}
+
+test_status test_document(FILE* fp) {
+    print_running("test_document");
+    test_document1(fp);
     return TEST_OK;
 }
--- lab1/src/test/test_runner.c
+++ lab1/src/test/test_runner.c
@@ -5,6 +5,7 @@
 int main() {
     FILE* fp = fopen("tmp", "w");
     test_element(fp);
+    fp = fopen("tmp", "w");
     test_document(fp);
     return 0;
 }
